ef4dc96d6a8b7570fea406400ab4a495e6ac0f5c
jaymeandretta <jayme.andretta@gmail.com>
2022-12-12 21:45:35 -0300

    config

--- server.js
+++ server.js
@@ -3,4 +3,7 @@ const { PORT } = require("./config.js");
 
 let app = express();
 app.use(express.static("wwwroot"));
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
+app.use(require("./routes/auth.js"));
+app.listen(PORT, function () {
+  console.log(`Server listening on port ${PORT}...`);
+});
