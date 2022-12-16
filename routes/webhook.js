1f91bfd106b7c4f8b5aa0083b32c315eef727ac5
jaymeandretta <jayme.andretta@gmail.com>
2022-12-16 14:28:10 -0300

    Update webhook.js

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,117 +1,63 @@
 const express = require("express");
-// const { webhookMiddleware } = require("../services/aps.js");
 const bodyParser = require("body-parser");
+const {
+  authRefreshMiddleware,
+  getHubs,
+  getProjects,
+  getProjectContents,
+  getItemVersions,
+  postWorkitem,
+  getInternalToken,
+  getItemVersion,
+} = require("../services/aps.js");
+const { APS_BUCKET_KEY, ACTIVITY_ID } = require("../config.js");
+
 let router = express.Router();
 router.use("/api/webhooks", bodyParser.json());
 router.post("/api/webhooks", async function (req, res, next) {
   try {
-    res.json(req);
-    let projectId = req.body.payload.project;
-    let userID = req.body.payload.user_info.id;
-    let versionUrn = req.body.payload.source;
+    let two_legged_token = await getInternalToken();
+    let three_legged_token =
+      "eyJhbGciOiJSUzI1NiIsImtpZCI6IlU3c0dGRldUTzlBekNhSzBqZURRM2dQZXBURVdWN2VhIn0.eyJzY29wZSI6WyJkYXRhOndyaXRlIiwiZGF0YTpjcmVhdGUiLCJkYXRhOnJlYWQiLCJidWNrZXQ6cmVhZCIsImJ1Y2tldDp1cGRhdGUiLCJidWNrZXQ6ZGVsZXRlIiwiYnVja2V0OmNyZWF0ZSIsImNvZGU6YWxsIiwidmlld2FibGVzOnJlYWQiXSwiY2xpZW50X2lkIjoiOXFHMjFRbkZVSW53NE94cUtQZ1d5RnV0NmQ4d3FNcm8iLCJhdWQiOiJodHRwczovL2F1dG9kZXNrLmNvbS9hdWQvYWp3dGV4cDYwIiwianRpIjoibUthYnhlZU5nS2J0amFQSGRBYzhLZENMbzF6Qm9UUUJFaWNMZnBJeUZFeUJHeGhYTmhPNlVRSXdUNXZQRTkyaSIsInVzZXJpZCI6IjRaNkpCNkFDUllXTEQ5QUsiLCJleHAiOjE2NzEyMDMyMDB9.A-y3SV3QeUuuriyy9756DdvTezlUJsKMEtmS9JO6yDvwSrfv09xQryaO9EquIgdzAljTOQB8ELJbOj87GIaBePx7wMGMrNTkex25NpHiwQs44tQAYdYf2qTc9BtvtnWcC8VBmEkuzVk11Z0mp6DtprO5Wqg8j8bcMUnmQSY3rHb3NLhsPukO1FuJugwfV3VYbLPKgGt8DjrKohmPZwNTFAfQ-DefBPkRTH7RW6P8vNb7JxbEIcU0aQTSsOeilT8qf7rIXEQ21aZCilL9iSl5r34varNf6BehM0tuoAlmsG9G4dzfom2JZHwVmVvuLI1P5JPDsIuz72627ogzyob3WQ";
+    let project_id = req.body.payload.project;
+    let source_version_urn = req.body.resourceUrn;
+    let input_file_urn = await getItemVersion(
+      project_id,
+      source_version_urn,
+      two_legged_token.access_token
+    );
+    let input_file_name = req.body.payload["custom-metadata"].fileName;
+    let output_file_urn = `urn:adsk.objects:os.object:${APS_BUCKET_KEY}/${
+      input_file_name.slice(0, -4) + req.body.payload.version
+    }.json`;
+    let user_Id = req.body.payload.modifiedBy;
+    let coordinates_csv_urn = `urn:adsk.objects:os.object:${APS_BUCKET_KEY}/TabelaOrigens.csv`;
+    let tolerance = 0.1;
+    let hub_id = "";
+    let inputParamsJson = {
+      userId: user_Id,
+      versionUrn: source_version_urn,
+      projectId: project_id,
+      hubId: "",
+      tolerance: 0.1,
+      token: three_legged_token,
+      issueSubTypeId: "5a297e94-0a20-41d4-919c-1cb5dfe0bb96",
+      fileName: input_file_name,
+      outputUrn: output_file_urn,
+    };
+    postWorkitem(
+      ACTIVITY_ID,
+      two_legged_token.access_token,
+      three_legged_token,
+      input_file_urn,
+      output_file_urn,
+      coordinates_csv_urn,
+      inputParamsJson
+    );
+    res.json("JOB STARTED!");
   } catch (err) {
     next(err);
   }
 });
-
-function importExcel(
-  inputRvtS3Url,
-  inputExcS3Url,
-  inputJson,
-  outputRvtS3Url,
-  projectId,
-  createVersionBody,
-  access_token_3Legged,
-  access_token_2Legged
-) {
-  return new Promise(function (resolve, reject) {
-    const workitemBody = {
-      activityId:
-        designAutomation.nickname +
-        "." +
-        designAutomation.activity_name +
-        "+" +
-        designAutomation.appbundle_activity_alias,
-      arguments: {
-        inputFile: {
-          url: inputRvtS3Url,
-          headers: {
-            Authorization: "Bearer " + access_token_3Legged.access_token,
-          },
-        },
-        inputJson: {
-          url: "data:application/json," + JSON.stringify(inputJson),
-        },
-        inputXls: {
-          url: inputExcS3Url,
-          headers: {
-            Authorization: "Bearer " + access_token_2Legged.access_token,
-          },
-        },
-
-        outputRvt: {
-          verb: "put",
-          url: outputRvtS3Url,
-          headers: {
-            Authorization: "Bearer " + access_token_3Legged.access_token,
-          },
-        },
-        onComplete: {
-          verb: "post",
-          url: designAutomation.webhook_url,
-        },
-      },
-    };
-
-    var options = {
-      method: "POST",
-      url: designAutomation.endpoint + "workitems",
-      headers: {
-        Authorization: "Bearer " + access_token_2Legged.access_token,
-        "Content-Type": "application/json",
-      },
-      body: workitemBody,
-      json: true,
-    };
-
-    request(options, function (error, response, body) {
-      if (error) {
-        reject(error);
-      } else {
-        let resp;
-        try {
-          resp = JSON.parse(body);
-        } catch (e) {
-          resp = body;
-        }
-        workitemList.push({
-          workitemId: resp.id,
-          projectId: projectId,
-          createVersionData: createVersionBody,
-          access_token_3Legged: access_token_3Legged,
-        });
-
-        if (response.statusCode >= 400) {
-          console.log(
-            "error code: " +
-              response.statusCode +
-              " response message: " +
-              response.statusMessage
-          );
-          reject({
-            statusCode: response.statusCode,
-            statusMessage: response.statusMessage,
-          });
-        } else {
-          resolve({
-            statusCode: response.statusCode,
-            headers: response.headers,
-            body: resp,
-          });
-        }
-      }
-    });
-  });
-}
 
 module.exports = router;
